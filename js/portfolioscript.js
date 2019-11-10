2ade5ed1f34b1d5a4215892fbe260ebf64a3e554
Olajide Jegede <33847734+ojjegede@users.noreply.github.com>
2019-11-09 19:38:21 -0500

    Update portfolioscript.js

--- js/portfolioscript.js
+++ js/portfolioscript.js
@@ -32,15 +32,3 @@ function myFunction() {
 window.onload = function() {
   typeWriter();
 };
-
-var i = 0;
-var txt = 'a travel diary';
-var speed = 70;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("type").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
